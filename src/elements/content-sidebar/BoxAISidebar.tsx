e6182127b6bfeed160125dacdbf79ae59786262e
MateuszMamczarz <mamczarz.mat@gmail.com>
2024-12-05 14:47:39 +0100

    feat(boxai-sidebar): Add context for passing props over withApiWrapper

--- src/elements/content-sidebar/BoxAISidebar.tsx
+++ src/elements/content-sidebar/BoxAISidebar.tsx
@@ -1,13 +1,11 @@
 /**
- * @file Redesigned Metadata sidebar component
+ * @file Box AI sidebar component
  * @author Box
  */
 import * as React from 'react';
 import flow from 'lodash/flow';
 import { useIntl } from 'react-intl';
 
-import { ArrowsExpand } from '@box/blueprint-web-assets/icons/Fill';
-import { IconButton } from '@box/blueprint-web';
 import { BoxAiContentAnswers, withApiWrapper, type ApiWrapperProps } from '@box/box-ai-content-answers'
 import SidebarContent from './SidebarContent';
 import { withAPIContext } from '../common/api-context';
@@ -16,10 +14,9 @@ import { withLogger } from '../common/logger';
 import { ORIGIN_BOXAI_SIDEBAR, SIDEBAR_VIEW_BOXAI } from '../../constants';
 import { EVENT_JS_READY } from '../common/logger/constants';
 import { mark } from '../../utils/performance';
-import type { User } from '../../common/types/core';
+import { BoxAISidebarContext } from './BoxAISidebarContainer';
 
 import messages from '../common/messages';
-import sidebarMessages from './messages';
 
 import './BoxAISidebar.scss';
 
@@ -27,18 +24,10 @@ const MARK_NAME_JS_READY: string = `${ORIGIN_BOXAI_SIDEBAR}_${EVENT_JS_READY}`;
 
 mark(MARK_NAME_JS_READY);
 
-
-export interface BoxAISidebarProps extends ApiWrapperProps {
-    currentUser: User,
-    onExpandClick: () => void,
-    fileName: string,
-    contentType: string,
-}
-
-function BoxAISidebar({ currentUser, onExpandClick, fileName, ...props }: BoxAISidebarProps) {
+function BoxAISidebar(props: ApiWrapperProps) {
     const { formatMessage } = useIntl();
-    const { createSession, encodedSession, sendQuestion } = props;
-    const userInfo = { name: currentUser.name, avatarUrl: currentUser.avatar_url };
+    const { elementId, userInfo, contentName } = React.useContext(BoxAISidebarContext);
+    const { createSession, encodedSession, sendQuestion, ...rest } = props;
 
     React.useEffect(() => {
         if (!encodedSession && createSession) {
@@ -48,15 +37,8 @@ function BoxAISidebar({ currentUser, onExpandClick, fileName, ...props }: BoxAIS
 
     return (
         <SidebarContent
-            actions={
-                <IconButton
-                    aria-label={formatMessage(sidebarMessages.boxAISidebarExpand)}
-                    icon={ArrowsExpand}
-                    onClick={onExpandClick}
-                    size="x-small"
-                />
-            }
             className="bcs-BoxAISidebar"
+            elementId={elementId}
             sidebarView={SIDEBAR_VIEW_BOXAI}
             title={formatMessage(messages.sidebarBoxAITitle)}
         >
@@ -65,10 +47,10 @@ function BoxAISidebar({ currentUser, onExpandClick, fileName, ...props }: BoxAIS
                     userInfo={userInfo} 
                     className="bcs-BoxAISidebar-contentAnswers" 
                     isSidebarOpen 
-                    contentName={fileName} 
+                    contentName={contentName} 
                     contentType={props.contentType} 
                     submitQuestion={sendQuestion} 
-                    {...props} 
+                    {...rest} 
                 />
             </div>
         </SidebarContent>
@@ -78,10 +60,10 @@ function BoxAISidebar({ currentUser, onExpandClick, fileName, ...props }: BoxAIS
 export { BoxAISidebar as BoxAISidebarComponent };
 
 const BoxAISidebarDefaultExport: typeof withAPIContext = flow([
-    withApiWrapper,
     withLogger(ORIGIN_BOXAI_SIDEBAR),
     withErrorBoundary(ORIGIN_BOXAI_SIDEBAR),
     withAPIContext,
+    withApiWrapper, // returns only props for Box AI, keep it at the end
 ])(BoxAISidebar);
 
 export default BoxAISidebarDefaultExport;
